b1af252d088e88fc74478156c8f9949207e75df7
PrasiddhaKhadka <prasiddhak77@gmail.com>
2026-02-20 09:36:45 +0545

    Add: Auth Database Intialized

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -1,11 +1,68 @@
 import app from './app.js'
 import dotenv from 'dotenv'
+import {sql} from './utils/db.js'
 
 
 dotenv.config()
 
+
+async function initDb(){
+    try {
+        await sql`
+        DO $$
+        BEGIN
+            IF NOT EXISTS(SELECT 1 FROM pg_type WHERE typname='user_role') THEN
+            CREATE TYPE user_role as ENUM ('jobseeker','recruiter');
+            END IF;
+        END $$
+        `
+
+        await sql`
+        CREATE TABLE IF NOT EXISTS users(
+            user_id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL UNIQUE,
+            password VARCHAR(255) NOT NULL,
+            phone_number VARCHAR(20) NOT NULL,
+            role user_role NOT NULL,
+            bio TEXT,
+            resume VARCHAR(255),
+            resume_public_id VARCHAR(255),
+            profile_pic VARCHAR(255),
+            profile_pic_public_id VARCHAR(255),
+            created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            subscription TIMESTAMPTZ
+        )
+        `;
+
+        await sql`
+        CREATE TABLE IF NOT EXISTS skills(
+            skill_id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL UNIQUE
+        )
+        `;
+
+
+        await sql`
+        CREATE TABLE IF NOT EXISTS user_skills(
+           user_id INTEGER NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+           skills_id INTEGER NOT NULL REFERENCES skills(skill_id) ON DELETE CASCADE,
+           PRIMARY KEY(user_id, skills_id)
+        )
+        `;
+
+        console.log(" 🏄‍♂️ DATABASE CREATED SUCCESSFULLY ")
+
+    } catch (error) {
+        console.log('Error Initializing Database',error)
+        process.exit(1);
+    }
+}
+
 const PORT = process.env.PORT || 8000
 
 app.listen(PORT,()=>{
-    console.log(`Auth Service is running on http://localhost:${PORT}`)
+    initDb().then(()=>{
+         console.log(`Auth Service is running on http://localhost:${PORT}`)
+    })
 })
