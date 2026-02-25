d7c99c84f662c55ee012c102d0182497e08e4884
PrasiddhaKhadka <prasiddhak77@gmail.com>
2026-02-25 10:11:54 +0545

    Fix:User Routes

--- services/user/src/routes/user.routes.ts
+++ services/user/src/routes/user.routes.ts
@@ -1,4 +1,4 @@
-import { getUserProfile, myProfile, updateResume, updateUserProfile } from '../controllers/user.controller.js';
+import { addSkills, deleteSkillFromUser, getUserProfile, myProfile, updateResume, updateUserProfile } from '../controllers/user.controller.js';
 import { isAuth } from '../middleware/auth.js';
 import express from 'express'
 import uploadFile from '../middleware/multer.js';
@@ -12,6 +12,10 @@ router.get('/:userId',isAuth,getUserProfile);
 router.put('/update/:userId',isAuth,updateUserProfile);
 router.put('/update/pic', isAuth, uploadFile, updateUserProfile)
 router.put('/update/resume', isAuth, uploadFile, updateResume)
+router.put('/skill/add', isAuth, addSkills)
+router.put('/skill/delete', isAuth, deleteSkillFromUser)
+
+
 
 
 
